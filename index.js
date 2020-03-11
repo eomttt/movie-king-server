79bf9ee7f6e840b9b79829b56778a22f3cc4897c
eomttt <hyunt0413@gmail.com>
2020-03-11 10:03:39 +0900

    Add option

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { resolvers } = require('./graphql/resolvers');
 
+console.log('Process node version', process.version);
+
+const PORT = process.env.PORT;
+
 const server = new GraphQLServer({
     typeDefs: "graphql/scheme.graphql",
     resolvers 
-})
+});
 
-server.start(() => console.log("Graphql Server Running"))
+const options = {
+    port: PORT || 4000,
+    endpoint: '/graphql',
+    subscriptions: '/subscriptions',
+    playground: '/playground',
+}
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+)
