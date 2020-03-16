5b545abd1fccb88f2a50aa971973a3debc8a3404
eomttt <hyunt0413@gmail.com>
2020-03-16 17:22:51 +0900

    Test for git

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const options = {
     subscriptions: '/subscriptions',
     playground: '/playground',
 }
+
 server.start(options, ({ port }) =>
   console.log(
     `Server started, listening on port ${port} for incoming requests.`,
